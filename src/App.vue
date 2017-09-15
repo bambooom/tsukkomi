c31cf33a98b2c588c8cc73389d412d9816abec00
bambooom <nmz89825@gmail.com>
2017-09-15 11:49:29 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>💁🏼</h1>
+    <Top></Top>
+  </div>
+</template>
+
+<script>
+import Top from './components/Top.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Top,
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Top.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="top-header">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header',
+  data () {
+    return {
+      msg: 'Just 不吐槽不酥胡',
+    };
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+/*ul {*/
+  /*list-style-type: none;*/
+  /*padding: 0;*/
+/*}*/
+
+/*li {*/
+  /*display: inline-block;*/
+  /*margin: 0 10px;*/
+/*}*/
+
+a {
+  color: #42b983;
+}
+</style>
